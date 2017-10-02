866f0ac11cb1ade935b3743c570bea1e39cac244
vbaghdas <vbaghdas@gmail.com>
2017-10-02 14:08:43 -0700

    redirected script source

--- calculator.js
+++ calculator.js
@@ -1,181 +1,122 @@
-$(document).ready(init);
+$(document).ready(applyClickHandlers)
 
-var my_calculator = null;
-
-function init() {
-    my_calculator = new Calculator();
-    my_calculator.set_options({
-        body: '.calculator',
-        numbers: '.number',
-        operators: '.operator',
-        equals: '#equalSign',
-        display: '.display'
-    });
-    my_calculator.initialize();
+function applyClickHandlers() {
+    $('.calc').on('keydown', keydown);
+    $('.calc').on('click', '.number', number);
+    $('.calc').on('click', '.operator', operator);
+    $('.calc').on('click', '.calculate', calculate);
+    $('.calc').on('click', '.clearEntry', clearEntry);
+    $('.calc').on('click', '.clear', clear);
 }
 
-function Calculator() {
-    this.inputs = [];
-    this.current_input_index = 0;
-    this.calc_parts = {
-        body: null,
-        numbers: null,
-        operators: null,
-        equals: null,
-        display: null
-    };
-    this.debug_mode = 'verbose';
+var calculation = [];
 
-    this.initialize = function() {
-        this.attach_click_handlers();
-    };
-    this.attach_click_handlers = function (){
-        this.calc_parts.numbers.click(this.handle_number_click.bind(this));
-        this.calc_parts.operators.click(this.handle_operator_click.bind(this));
-    };
-    this.handle_operator_click = function(){
-        this.display_error('in operator click handler');
-        var button_text = $(event.target).text();
-        this.current_input_index++;
-        var op = new operator_obj;
-        op.set_value(button_text);
-        this.inputs.push(op);
-        this.current_input_index++;
-    };
-    this.handle_number_click = function (){
-        this.display_error('in number click handler');
-        var button_text = $(event.target).text();
-        if(this.inputs[this.current_input_index] === undefined){
-            var node = new number_obj(this);
-            this.inputs.push(node);
-        }
-        this.inputs[this.current_input_index].add_value(button_text);
-    };
-    this.display_error = function(message, type){
-        if(this.debug_mode !== 'verbose'){
-            return
-        }
-        if(type === undefined){
-            type = 'warning';
-        }
-        if(type === 'warning'){
-            console.warn(message);
-        }
-        else {
-            console.error(message);
-        }
-    };
-    this.set_options = function (options){
-        for(var i in options){
-            var temp_name = 'set_calc_' + i;
-            if(this[temp_name] !== undefined){
-                this[temp_name](options[i]);
-            } else {
-                this.display_error('that option is not valid')
+function doMath(array) {
+    var result = 0;
+    var item = 0;
+    for (var index = 0; index < array.length; index++) {
+        if (isNaN(array[index])) {
+            switch(array[index]) {
+                case 'x':
+                    item = array[index-1] * array[index+1];
+                    index--;
+                    array.splice(index, 3, item);
+                    break;
+                case '/':
+                    item = array[index-1] / array[index+1];
+                    index--;
+                    array.splice(i, 3, item);
+                    break;
+                default:
+                    break;
             }
         }
-    };
-    this.set_calc_body = function (element){
-        this.calc_parts.body = $(element);
-    };
-    this.set_calc_display = function(element){
-        this.calc_parts.display = $(element);
-    };
-    this.set_calc_numbers = function(element){
-        this.calc_parts.numbers = $(element);
-    };
-    this.set_calc_operators = function(element){
-        this.calc_parts.operators = $(element);
-    };
-    this.set_calc_equals = function(element){
-        this.calc_parts.equals = $(element);
+    }
+    for (var index = 0; index < array.length; index++) {
+        if (isNaN(array[index])) {
+            switch(array[index]) {
+                case '-':
+                    item = array[index-1] - array[index+1];
+                    index--;
+                    array.splice(index, 3, item);
+                    break;
+                case '+':
+                    item = array[index-1] + array[index+1];
+                    index--;
+                    array.splice(index, 3, item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    result = array[0];
+    return result;
+}
+
+function number() {
+    $('.displayLarge').append($(this).val());
+}
+
+function operator() {
+    if ($('.displayLarge').text() === "") {
+        var currentOperator = $(this).val();
+        calculation.pop();
+        calculation.push(currentOperator);
+        var currentInput = $('.displaySmall').text();
+        var previousInput = currentInput.substr(0, (currentInput.length - 2));
+        $('.displaySmall').text(previousInput).append(currentOperator);
+    } else {
+        var text = parseFloat($('.displayLarge').text());
+        currentOperator = $(this).val();
+        $('.displaySmall').append(text + " " + currentOperator + " ");
+        $('.displayLarge').text('');
+        calculation.push(text, currentOperator);
     }
 }
 
-function number_obj(parent) {
-    this.value = null;
-    this.parent = parent;
-    this.type = 'number';
-    this.has_decimal = false;
-    this.set_value = function(new_value){
-        this.value = new_value;
-    };
-    this.display_error = function(message){
-        this.parent.display_error(message)
-    };
-    this.add_value = function(new_value){
-        if(new_value !== '.' && !this.test_if_number(new_value)){
-            this.display_error('given value is not a number');
-            return false;
+function calculate() {
+    var result = 0;
+    if ($('.displayLarge').text() === "") {
+        if (calculation.length !== 0) {
+            calculation.pop();
+            result = doMath(calculation);
+            calculation = [];
+            $('.displaySmall').text('');
+            $('.displayLarge').text(result);
         }
-        if(this.value === null){
-            this.value = new_value;
-        } else {
-            if(new_value === '.'){
-                if(this.has_decimal){
-                    this.display_error('already has a decimal');
-                    return false;
-                } else {
-                    this.has_decimal = true;
-                }
-            }
-            this.value += new_value.toString();
-        }
-        return this.get_value();
-    };
-    this.get_value = function(){
-        return parseFloat(this.value);
-    };
-    this.test_if_number = function(unknown_value){
-        if(!isNaN(unknown_value)){
-            return true;
-        } else {
-            return false;
-        }
+    } else {
+        var text = parseFloat($('.displayLarge').text());
+        calculation.push(text);
+        result = doMath(calculation);
+        calculation = [];
+        $('.displaySmall').text('');
+        $('.displayLarge').text(result);
     }
 }
 
-function operator_obj(parent) {
-    this.value = null;
-    this.type = 'operator';
-    this.parent = parent;
-    this.set_value = function(new_value){
-        this.value = new_value;
-    };
-    this.get_value = function(){
-        return this.get_value();
-    };
-    this.display_error = function(message){
-        this.parent.display_error(message);
-    };
-    this.do_math = function(operand1_object, operand2_object){
-        var operand1 = operand1_object.get_value();
-        var operand2 = operand2_object.get_value();
-        switch(this.value){
-            case '+':
-            case 't':
-                return operand1 + operand2;
-                break;
-            case '-':
-            case '_':
-                return operand1 - operand2;
-                break;
-            case '/':
-            case '%':
-                if(operand2 === 0){
-                    this.display_error('cannot divide by 2');
-                    return
-                }
-                else if(operand2 === 2){
-                    return operand1 >> 1;
-                }
-                return operand1 / operand2;
-                break;
-            case 'x':
-            case 'X':
-            case '*':
-                return operand1 * operand2;
-                break;
-        }
+function clearEntry() {
+    var currentInput = $('.displayLarge').text();
+    var previousInput = currentInput.substr(0, (currentInput.length - 1));
+    $('.displayLarge').text(previousInput);
+}
+
+function clear() {
+    $('.displaySmall').text('');
+    $('.displayLarge').text('');
+    calculation = [];
+}
+
+function keydown() {
+    var clicked = event.which || event.keyCode;
+    if (clicked === 46 || clicked === 110 || clicked === 190) {
+        $('.displaySmall').append(String.fromCharCode(46));
+    } else if (clicked === 13) {
+        calculateClicked();
+    } else if (clicked === 8) {
+        clear();
+    }
+    else if (clicked >= 48 && clicked <= 57) {
+        $('.displayLarge').append(String.fromCharCode(clicked));
     }
 }
